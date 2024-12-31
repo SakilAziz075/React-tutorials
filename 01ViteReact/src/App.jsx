ae0d7baadc176be0c915b0352417edd95ed01a55
SakilAziz075 <sakilaziz12@gmail.com>
2025-01-01 00:59:45 +0530

    kiba aru

---
+++ 01ViteReact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+      <Chai />
+      <p>Testing Fragment</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01ViteReact/src/chai.jsx
@@ -0,0 +1,11 @@
+//COMPONENT NAMES MUST BE IN UPPERCASE
+
+function Chai(){
+    return(
+        <h3>
+            chai is ready
+        </h3>
+    )
+}
+
+export default Chai;
